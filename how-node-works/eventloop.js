2e75b9be4b83bc8ca2c59eb5d05bd37bd0f846ff
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-20 22:46:13 +0300

    more on it

---
+++ how-node-works/eventloop.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+setTimeout(() => console.log('Timer 1 Finished'), 0);
+setImmediate(() => console.log('Immediate 1 finished'));
+
+fs.readFile('./test-file.txt', (err, data) => {
+  console.log('I/O finished');
+  setTimeout(() => console.log('Timer 2 Finished'), 0);
+  setTimeout(() => console.log('Timer 3 Finished'), 3000);
+  setImmediate(() => console.log('Immediate 2 finished'));
+});
+
+console.log('Hello from the top level code');
