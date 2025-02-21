91d66bde1b33a4ce321fb24c3f83a44669209b4f
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-21 23:10:53 +0300

    event loop

--- how-node-works/eventloop.js
+++ how-node-works/eventloop.js
@@ -5,9 +5,17 @@ setImmediate(() => console.log('Immediate 1 finished'));
 
 fs.readFile('./test-file.txt', (err, data) => {
   console.log('I/O finished');
+  console.log('-----------------------------------');
   setTimeout(() => console.log('Timer 2 Finished'), 0);
   setTimeout(() => console.log('Timer 3 Finished'), 3000);
   setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => console.log('Process.nextTick'));
 });
 
 console.log('Hello from the top level code');
+
+function call() {
+  console.log('call function');
+}
+call();
