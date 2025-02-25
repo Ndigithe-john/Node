963225d8d707b1bbad526c10e4aaa21cfb464c18
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-26 01:12:15 +0300

    reading file using createReadStream and sending it using write

---
+++ how-node-works/streams.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  //  solution 1
+  //   fs.readFile('./test-file.txt', 'utf-8', (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  // Solution 2: Use streams
+  const readable = fs.createReadStream('./test-file.txt');
+  readable.on('data', (chunk) => {
+    res.write(chunk);
+  });
+  readable.on('end', () => {
+    res.end();
+  });
+  readable.on('error', (err) => {
+    console.log(err);
+    res.statusCode = 500;
+    res.end('File not found');
+  });
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to port 8000......');
+});
