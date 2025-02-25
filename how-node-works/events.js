85c3307ccb3f6e2d8ad0e1b7793d618b1bde1d82
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-25 23:42:42 +0300

    events emitting

--- how-node-works/events.js
+++ how-node-works/events.js
@@ -1,6 +1,12 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmmiter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+const myEmmiter = new Sales();
 
 myEmmiter.on('newSale', () => {
   console.log('New sale recorded');
@@ -14,5 +20,25 @@ myEmmiter.on('newSale', (stock) => {
   console.log(`There are now ${stock} items left in stock`);
 });
 myEmmiter.emit('newSale', 9);
-myEmmiter.emit('newSale');
-myEmmiter.emit('newSale');
+
+///////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('2 Request Received');
+  console.log(req.url);
+  res.end('Request Received');
+});
+
+server.on('request', (req, res) => {
+  console.log('Another Request 😂');
+});
+
+server.on('close', () => {
+  console.log('Server Close');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Waiting for requests');
+});
