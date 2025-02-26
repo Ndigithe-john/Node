c56b852febb704bdb70d600e1ecaf3a479b6df7e
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-26 19:07:41 +0300

    added the promise.all to await multiple promises

--- asyncronous-js/index.js
+++ asyncronous-js/index.js
@@ -24,12 +24,20 @@ const getDogPic = async () => {
     const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superAgent.get(
+    const res1Pro = superAgent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
+    const res2Pro = superAgent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Pro = superAgent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
 
-    await writeFilePro('dog-img.txt', res.body.message);
+    const all = await Promise.all([res1Pro, res2Pro, res3Pro]);
+    const imgs = all.map((el) => el.body.message);
+
+    await writeFilePro('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file');
   } catch (error) {
     console.log(error);
