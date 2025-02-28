edf84e2d7a9cefac0846525ec00a33a6846a939c
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-28 12:45:28 +0300

    the get request for tout tour

--- natours/server.js
+++ natours/server.js
@@ -1,14 +1,28 @@
+const fs = require('fs');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Changing the world', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Changing the world', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoing');
-});
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoing');
+// });
 const port = 8000;
 
 app.listen(port, () => {
