351981edf7bad476963093067e7c12ae32b52543
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-28 14:59:41 +0300

    specifying parameters in a url and reading them

--- natours/server.js
+++ natours/server.js
@@ -19,24 +19,64 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+
+  const tour = tours.find((el) => el.id === +id);
+  // if (+id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  // console.log(req);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour);
+  // tours.push(newTour)
+  // tours = [...tours, newTour]; if we use let we can modify tours
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
-      console.error(err);
+      console.log(err);
       res.status(201).json({
         status: 'success',
-        data: { tour: newTour },
+        data: {
+          tours: newTour,
+        },
       });
     }
   );
 });
+
+// app.post('/api/v1/tours', (req, res) => {
+//   // console.log(req);
+//   const newId = tours[tours.length - 1].id + 1;
+//   const newTour = Object.assign({ id: newId }, req.body);
+
+//   tours.push(newTour);
+//   fs.writeFile(
+//     `${__dirname}/dev-data/data/tours-simple.json`,
+//     JSON.stringify(tours),
+//     (err) => {
+//       console.error(err);
+//       res.status(201).json({
+//         status: 'success',
+//         data: { tour: newTour },
+//       });
+//     }
+//   );
+// });
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Changing the world', app: 'Natours' });
