8b432d1b32ed2a22201dff80fc467c04634a2ce9
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-28 13:31:56 +0300

    added the post route to add a tour

--- natours/server.js
+++ natours/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -16,6 +18,26 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      console.error(err);
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
+});
+
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'Changing the world', app: 'Natours' });
 // });
