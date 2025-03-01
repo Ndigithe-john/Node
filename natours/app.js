3eea3a22f4933c35f25551c7403595ba57a87bdd
Ndigithe-john <ndigithejohn@gmail.com>
2025-03-01 13:29:33 +0300

    added the param middleware to check for id validity

---
+++ natours/app.js
@@ -0,0 +1,28 @@
+const colors = require('colors');
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware'.bgRed);
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// Routes
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -3,6 +3,18 @@ const fs = require('fs');
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
+
+exports.checkId = (req, res, next, val) => {
+  console.log(`The id is ${val}`);
+  const { id } = req.params;
+  if (+id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -19,13 +31,7 @@ exports.getTour = (req, res) => {
   const { id } = req.params;
 
   const tour = tours.find((el) => el.id === +id);
-  // if (+id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid Id',
-    });
-  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -38,10 +44,10 @@ exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
-  // tours.push(newTour)
+  tours.push(newTour);
   // tours = [...tours, newTour]; if we use let we can modify tours
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       console.log(err);
@@ -56,15 +62,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  const { id } = req.params;
-  // const tour = tours.find((el) => el.id === +id);
-  // if (!tour) {
-  if (+id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -73,14 +70,6 @@ exports.updateTour = (req, res) => {
   });
 };
 exports.deleteTour = (req, res) => {
-  const { id } = req.params;
-
-  if (+id > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null,
