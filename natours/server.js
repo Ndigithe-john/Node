8a3f9019da883aa150b43132c7051541d195f95e
Ndigithe-john <ndigithejohn@gmail.com>
2025-03-02 23:36:02 +0300

    prettier and eslint to manage my warnings and errors

--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 8000;
+// console.log(app.get('env'));----This one is for express
+// console.log(process.env);----This is for node core
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.......`.bold.bgYellow);
