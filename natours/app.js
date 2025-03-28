6fe84b040d1378770413d84a0f3ed84f7ac9ff43
Ndigithe-john <ndigithejohn@gmail.com>
2025-03-28 22:10:16 +0300

    added the unhandled routes middleware

--- natours/app.js
+++ natours/app.js
@@ -17,11 +17,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware'.bgRed);
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -29,5 +24,12 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
