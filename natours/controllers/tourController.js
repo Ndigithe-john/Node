ce4255c2939d8156f120ea27af6531bbf3da0474
Ndigithe-john <ndigithejohn@gmail.com>
2025-03-30 22:08:44 +0300

    more on error handling

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -84,6 +84,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
       runValidators: true,
     },
   );
+
+  if (!updatedTour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -93,7 +97,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await TourModel.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that id', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: null,
