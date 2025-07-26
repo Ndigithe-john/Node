1fc7d81faf94dd19594d5b12216d6d986ca7372e
Ndigithe-john <ndigithejohn@gmail.com>
2025-07-26 23:44:13 +0300

    removing unnecessary middlewares

--- natours/app.js
+++ natours/app.js
@@ -7,26 +7,16 @@ const morgan = require('morgan');
 const app = express();
 
 // Middlewares
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 // Route handlers
 const getAllTours = (req, res) => {
-  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
