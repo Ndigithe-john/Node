2489294d06a9163f0b5473ea11c83621b105e5c1
Ndigithe-john <ndigithejohn@gmail.com>
2025-08-02 23:28:37 +0300

    Made the database connection

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DB_CONNECTION_STRING.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD,
+);
+console.log(DB);
+// mongoose
+//   .connect(DB, {
+//     // useNewURLParser: true,
+//     // useCreateIndex: true,
+//     // useFindAndModify: false,
+//   })
+//   .then((conn) => {
+//     console.log(conn.connections);
+//   });
+
+mongoose.connect(DB).then(() => {
+  console.log('Database connected successfully!'.bgGreen);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
