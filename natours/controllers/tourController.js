64082b33310a806c6ebc95c05e84bc4e40f586a1
Ndigithe-john <ndigithejohn@gmail.com>
2025-09-06 00:07:20 +0300

    impemented my filter to ignore page, sort, limit and fields in my req.query

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -33,13 +33,18 @@ const createTour = async (req, res) => {
  */
 const getAllTours = async (req, res) => {
   try {
+    const queryObj = { ...req.query };
+    const exludedFields = ['page', 'sort', 'limit', 'fields'];
+    exludedFields.forEach((el) => delete queryObj[el]);
+
     console.log(req.query);
+    console.log(queryObj);
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-    const tours = await Tour.find({ duration: 5, difficulty: 'easy' });
+    const tours = await Tour.find(queryObj);
 
     res.status(200).json({
       status: 'success',
